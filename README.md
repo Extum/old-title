f0de932fe68fbae381d5cdc32d862fdd13f6dfe5
Joel Bond <joel.bond@partialdev.com>
2018-08-02 16:07:05 -0600

    Extension rewrite

---
+++ README.md
@@ -0,0 +1,26 @@
+# Old Title
+
+---
+
+![License](https://img.shields.io/badge/license-MIT-blue.svg) [![Latest Stable Version](https://img.shields.io/packagist/v/extum/flarum-ext-old-title.svg)](https://packagist.org/packages/extum/flarum-ext-old-title)
+
+A [Flarum](http://flarum.org) extension. 
+
+### Installation
+
+Use [Bazaar](https://discuss.flarum.org/d/5151-flagrow-bazaar-the-extension-marketplace) or install manually with composer:
+
+```sh
+composer require extum/flarum-ext-old-title
+```
+
+### Updating
+
+```sh
+composer update extum/flarum-ext-old-title
+php flarum cache:clear
+```
+
+### Links
+
+- [Packagist](https://packagist.org/packages/extum/flarum-ext-old-title)
---
+++ src/Listeners/AddClientAssets.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ *  This file is part of extum/flarum-ext-old-title.
+ *
+ *  Copyright (c) 2018 .
+ *
+ *
+ *  For the full copyright and license information, please view the LICENSE.md
+ *  file that was distributed with this source code.
+ */
+
+namespace Extum\OldTitle\Listeners;
+
+use DirectoryIterator;
+use Flarum\Event\ConfigureLocales;
+use Flarum\Event\ConfigureWebApp;
+use Illuminate\Contracts\Events\Dispatcher;
+
+class AddClientAssets
+{
+
+    /**
+     * Subscribes to the Flarum events.
+     *
+     * @param Dispatcher $events
+     */
+    public function subscribe(Dispatcher $events)
+    {
+        $events->listen(ConfigureWebApp::class, [$this, 'configureWebApp']);
+        
+    }
+    
+    /**
+     * Modifies the client view for forum/admin.
+     *
+     * @param ConfigureWebApp $event
+     */
+    public function configureWebApp(ConfigureWebApp $event)
+    {
+        
+
+        if ($event->isForum()) {
+            $event->addAssets([
+                
+                __DIR__.'/../../less/app.less',
+            ]);
+            $event->addBootstrapper('extum/flarum-ext-old-title/main');
+        }
+    }
+    
+}
