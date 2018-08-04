59b916c0b6f9c6ffc1c2d3c6ad021c50db6e9ffe
Arda Çebi <ardacebi1@gmail.com>
2018-08-04 18:52:01 +0300

    Commit

--- js/admin/src/main.js
+++
@@ -1,9 +0,0 @@
-import app from 'flarum/app';
-import {extend} from 'flarum/extend';
-import Page from 'flarum/components/Page';
-
-app.initializers.add('extum/flarum-ext-old-title', () => {
-  extend(Page.prototype, 'init', function () {
-        $('head').prepend();
-    });
-});
--- js/forum/src/main.js
+++
@@ -1,5 +0,0 @@
-import app from 'flarum/app';
-
-app.initializers.add('extum/flarum-ext-old-title', () => {
-  $('head').prepend('<style>article.DiscussionRenamedPost span[title]::before{display:block;content:attr(title)}</style>')
-});
